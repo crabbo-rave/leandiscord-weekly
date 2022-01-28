4518efde1064d79acc248c5bf1e443568ff7fb89
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-01-27 20:44:51 -0500

    Update crab-4.lean

--- Week 1/crab-4.lean
+++ Week 1/crab-4.lean
@@ -1,15 +1,15 @@
 /- Function for the programming question. -/
-def sumOfLongestSublistAux : List Nat → Nat → Nat → Nat → Nat → Nat 
-    | [], _, _, _, totalSum => totalSum
-    | [x], currLen, maxLen, currSum, totalSum => if currLen+1 > maxLen then currSum+x else totalSum
-    | x::y::xs, currLen, maxLen, currSum, totalSum =>
-        if y=x+1 then
-          sumOfLongestSublistAux (y::xs) (currLen+1) maxLen (currSum+x) totalSum
-        else
-          sumOfLongestSublistAux (y::xs) 0 (max currLen maxLen) 0 (if currLen > maxLen then currSum+x else totalSum)
+def sumOfLongestSublistAux : List Nat → (currLen maxLen currSum totalSum : Nat) → Nat 
+  | [], _, _, _, totalSum => totalSum
+  | [x], currLen, maxLen, currSum, totalSum => if currLen+1 > maxLen then currSum+x else totalSum
+  | x::y::xs, currLen, maxLen, currSum, totalSum =>
+      if y=x+1 then
+        sumOfLongestSublistAux (y::xs) (currLen+1) maxLen (currSum+x) totalSum
+      else
+        sumOfLongestSublistAux (y::xs) 0 (max currLen maxLen) 0 (if currLen > maxLen then currSum+x else totalSum)
 
 def sumOfLongestSublist (list: List Nat) : Nat := 
-    sumOfLongestSublistAux list 0 0 0 0
+  sumOfLongestSublistAux list (currLen := 0) (maxLen := 0) (currSum := 0) (totalSum := 0)
 
 #eval sumOfLongestSublist [1, 2, 3, 100, 7, 8, 9, 10] /- 34 -/
 #eval sumOfLongestSublist [1, 2, 3, 7, 4, 5, 6] /- 6 -/
