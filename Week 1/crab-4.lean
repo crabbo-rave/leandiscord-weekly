6fe4ed266e141d39713469632f309d840b7ed24c
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-01-27 20:05:09 -0500

    Update crab-4.lean

--- Week 1/crab-4.lean
+++ Week 1/crab-4.lean
@@ -1,14 +1,19 @@
 /- Function for the programming question. -/
 def sumOfLongestSublistAux : List Nat → Nat → Nat → Nat → Nat → Nat 
     | [], _, _, _, totalSum => totalSum
-    | [x], currLen, maxLen, currSum, totalSum => sumOfLongestSublistAux [] 0 0 0 (currSum+x)
+    | [x], currLen, maxLen, currSum, totalSum => if currLen+1 > maxLen then currSum+x else totalSum
     | x::y::xs, currLen, maxLen, currSum, totalSum =>
         if y=x+1 then
           sumOfLongestSublistAux (y::xs) (currLen+1) maxLen (currSum+x) totalSum
         else
-          sumOfLongestSublistAux (y::xs) 0 (max currLen maxLen) 0 (if currLen < maxLen then totalSum else currSum)
+          sumOfLongestSublistAux (y::xs) 0 (max currLen maxLen) 0 (if currLen > maxLen then currSum+x else totalSum)
 
 def sumOfLongestSublist (list: List Nat) : Nat := 
       sumOfLongestSublistAux list 0 0 0 0
 
-#eval sumOfLongestSublist [1,2,3,100,7,8,9,10] /- 34 -/
+#eval sumOfLongestSublist [1, 2, 3, 100, 7, 8, 9, 10] /- 34 -/
+#eval sumOfLongestSublist [1, 2, 3, 7, 4, 5, 6] /- 6 -/
+#eval sumOfLongestSublist [1, 2, 3, 7, 5, 6, 11, 12, 13] /- 6 -/
+#eval sumOfLongestSublist [5] /- 5 -/
+#eval sumOfLongestSublist [1,2,4] /- 3 -/
+#eval sumOfLongestSublist [] /- 0 -/
